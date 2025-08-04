cffa5b27859c8eac44a338815dffd74cf6a989c5
Brasonite <gabriel.silva.araujo@proton.me>
2025-08-04 13:33:10 -0300

    Add SceneManager

---
+++ Scenes/README.md
@@ -0,0 +1,7 @@
+<div align="center">
+
+![Quark Scenes banner](./banner.png)
+
+</div>
+
+This collection contains all utilities related to scenes and scene management.
---
+++ Scenes/SceneManager.gd
@@ -0,0 +1,47 @@
+## Version: 1.0
+##
+## Manages the flow of scenes in the game, facilitating background loading and transitions.
+## Must be used in place of [method SceneTree.change_scene_to_file] and [method SceneTree.change_scene_to_packed].
+## This module must be added as an autoload.
+##
+## This script is part the [Quark Collection](https://github.com/Brasonite/Quark) under the MIT license.
+
+class_name QuarkSceneManager extends Node
+
+signal started_loading(path: String)
+signal finished_loading(path: String)
+
+var current_scene_path := ""
+var currently_loading := ""
+
+func _init() -> void:
+	process_mode = Node.PROCESS_MODE_ALWAYS
+
+func _process(_delta: float) -> void:
+	if currently_loading.is_empty():
+		pass
+	
+	if ResourceLoader.load_threaded_get_status(currently_loading) == ResourceLoader.ThreadLoadStatus.THREAD_LOAD_LOADED:
+		switch_to_loaded()
+
+## Loads a scene in the background and switches to it once it's done.
+## Signals are emitted to notify the current loading state.
+func open(path: String) -> void:
+	if not currently_loading.is_empty():
+		printerr("Cannot load scene. Already loading '" + currently_loading + "'!")
+		return
+	
+	var status := ResourceLoader.load_threaded_request(path, "PackedScene")
+	if status != OK:
+		printerr("Couldn't load scene '" + path + "' (error " + str(status) + ").")
+		return
+	
+	currently_loading = path
+	started_loading.emit(path)
+
+func switch_to_loaded() -> void:
+	var scene: PackedScene = ResourceLoader.load_threaded_get(currently_loading)
+	get_tree().change_scene_to_packed(scene)
+	finished_loading.emit(currently_loading)
+	
+	currently_loading = ""
